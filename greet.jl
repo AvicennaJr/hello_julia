f6fdc96b58cbfa90bf1f4c1a93aadc0094920b0c
Avicenna <87450618+AvicennaJr@users.noreply.github.com>
2022-05-28 15:10:25 +0300

    Update greet.jl

--- greet.jl
+++ greet.jl
@@ -2,6 +2,9 @@ function greet()
     print("Hey there! What's your name? ")
     name = readline()
     println("Nice to meet you $name")
+    print("Where are you from $name? ")
+    place = readline()
+    println("I've heard good things about $place")
 end
 
 greet()
